cc6d782358745bb2bf027c91cc4e8aba38ec9701
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2024-11-01 22:55:02 -0400

    Add RPSResult framework

---
+++ ProjectsAssingment03/Assignment03.c
@@ -0,0 +1,9 @@
+#include "Assignment03.h"
+#include <stdio.h>
+
+
+//implementation
+
+const char* RPSResult(const char* player1Input, const char player2Input) {
+
+}
--- ProjectsAssingment03/Assignment03.h
+++ ProjectsAssingment03/Assignment03.h
@@ -1,1 +1,3 @@
 #pragma once
+//interfacing
+const char* RPSResult(const char* player1Input, const char* player2Input);
